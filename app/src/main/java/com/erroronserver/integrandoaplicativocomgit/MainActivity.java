eeeb96c705af8c449a9e199258ea0aa0a4c1060f
Raniere de Lima <ranieredelima@gmail.com>
2017-06-28 17:33:56 -0300

    Criando uma branch e modificando

--- app/src/main/java/com/erroronserver/integrandoaplicativocomgit/MainActivity.java
+++ app/src/main/java/com/erroronserver/integrandoaplicativocomgit/MainActivity.java
@@ -20,11 +20,17 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_posts:
+                    mTextMessage.setText(R.string.title_posts);
+                    return true;
+                case R.id.navigation_profile:
+                    mTextMessage.setText(R.string.title_profile);
                     return true;
             }
             return false;
@@ -40,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setSelectedItemId(R.id.navigation_dashboard);
     }
 
 }
